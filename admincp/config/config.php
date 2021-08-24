47f1c3c0527b4ed63750a9a8fd04abef629b4493
alannguyen2705 <55863320+alannguyen2705@users.noreply.github.com>
2021-08-24 15:02:50 +0700

    Update config.php

--- admincp/config/config.php
+++ admincp/config/config.php
@@ -1,6 +1,6 @@
 <?php 
 
-	$mysqli = new mysqli("sql6.freesqldatabase.com","sql6431813","Q6B9IGFRlL","webcard");
+	$mysqli = new mysqli("sql6.freesqldatabase.com","sql6431813","Q6B9IGFRlL","sql6431813");
 
 	// Check connection
 	if ($mysqli->connect_errno) {
