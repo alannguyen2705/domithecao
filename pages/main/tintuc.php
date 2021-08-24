4681a9f7eb4421799002fcb22a9c84d25c47ea47
alannguyen2705 <55863320+alannguyen2705@users.noreply.github.com>
2021-08-24 15:05:39 +0700

    Update tintuc.php

--- pages/main/tintuc.php
+++ pages/main/tintuc.php
@@ -25,7 +25,7 @@
 			<br>
 	<div style="width: 55%" class="main-container-1">
 		<div class="block-news">
-			<img src="../Assignment2/images/mobifone.jpg"> <br>
+			<img src="../images/mobifone.jpg"> <br>
 			<a href="https://mobifone3g.info/lich-khuyen-mai-mobifone-thang-10-2020-km-nap-the-hoa-mang-moi.html" target="_blank">October promotion calendar<br> Mobifone network operator</a>
 			<p>This item is Mobifone recharge information in October.<br>National promotional card recharge schedule for subscribers</p>
 		</div></div>
@@ -33,14 +33,14 @@
 
 	<div style="width: 55%" class="main-container-1">
 		<div class="block-news">
-			<img src="../Assignment2/images/Viettel.jpg"><br><br> 
+			<img src="../images/Viettel.jpg"><br><br> 
 			<a href="https://3gviettel.vn/khuyen-mai-viettel" target="_blank">October promotion calendar <br>  Viettel network operator</a>
 			<p>Will be continuously updated as soon as possible</p>
 		</div></div>
 		
 	<div style="width: 55%" class="main-container-1">
 		<div class="block-news">
-			<img src="../Assignment2/images/vina.jpg"> <br><br>
+			<img src="../images/vina.jpg"> <br><br>
 			<a href="https://dichvu3gvinaphone.vn/khuyen-mai-vinaphone/" target="_blank">October promotion calendar <br> Vinaphone network operator</a>
 			<p>Will be continuously updated as soon as possible</p>
 		</div></div>
